2a2149920c320b83bafd488029eb3f7af2028728
Jacob Wang <jatcwang@gmail.com>
2025-06-25 19:30:43 +0100

    more stuff to crossed

--- src/main/scala/io.github.jatcwang.iohandle/IOHandle.scala
+++ src/main/scala/io.github.jatcwang.iohandle/IOHandle.scala
@@ -32,3 +32,32 @@ class IOHandleImpl[E] private[iohandle] (marker: AnyRef) extends IOHandle[E] { s
 
 final private[iohandle] case class Submarine[E](e: E, marker: AnyRef) extends RuntimeException with NoStackTrace
 
+private[iohandle] class IOHandlePartiallyApplied[E](val handle: IOHandle[E]) extends AnyVal {
+  inline def apply[A](inline f: IOHandle[E] ?=> IO[A]): IOHandlePendingRescue[E, A] = new IOHandlePendingRescue(
+    convert(f),
+    handle,
+  )
+
+  private inline def convert[A, B](inline f: A ?=> B): A => B =
+    implicit a: A => f
+}
+
+private object impl {
+  def createIOHandle[E]: IOHandle[E] =
+    new IOHandleImpl[E](new AnyRef)
+}
+
+// TODO: eliminate allocation?
+private[iohandle] class IOHandlePendingRescue[E, A](
+  private val body: IOHandle[E] => IO[A],
+  private val ioHandle: IOHandle[E],
+) {
+
+  def rescueWith(handler: E => IO[A]): IO[A] = {
+    ioHandle.handleWith(body(ioHandle))(handler)
+  }
+
+  def toEither: IO[Either[E, A]] = {
+    ioHandle.handleWith(body(ioHandle).map(Right(_)))(e => IO.pure(Left(e)))
+  }
+}
